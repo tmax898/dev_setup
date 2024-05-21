0d433c269bf1b41045454a0b9186b0e1cfe947a0
tmax898 <tmax898@gmail.com>
2024-05-21 17:11:31 -0500

    add neotree configs

---
+++ nvim/lua/plugins/neo-tree.lua
@@ -0,0 +1,17 @@
+return {
+  "nvim-neo-tree/neo-tree.nvim",
+  opts = {
+    filesystem = {
+      group_empty_dirs = true,
+      follow_current_file = {
+        enabled = true,
+      },
+    },
+    buffers = {
+      group_empty_dirs = true,
+    },
+    window = {
+      auto_expand_width = true,
+    },
+  },
+}
