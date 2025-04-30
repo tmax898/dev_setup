f92b2543dc713818bd9abe50d4fe4b42395899de
Maxamillion <tmax898@gmail.com>
2025-04-29 20:56:57 -0500

    update astro to v5

--- nvim/lua/community.lua
+++ nvim/lua/community.lua
@@ -8,14 +8,18 @@
 return {
   "AstroNvim/astrocommunity",
   { import = "astrocommunity.pack.lua" },
+  { import = "astrocommunity.colorscheme.catppuccin" },
+  { import = "astrocommunity.colorscheme.night-owl-nvim" },
   { import = "astrocommunity.motion.nvim-surround" },
   { import = "astrocommunity.media.neocord-nvim" },
-  { import = "astrocommunity.pack.zig" },
+  -- { import = "astrocommunity.pack.zig" },
   { import = "astrocommunity.pack.elixir-phoenix" },
   { import = "astrocommunity.pack.typescript" },
   { import = "astrocommunity.pack.json" },
   { import = "astrocommunity.git.blame-nvim" },
   { import = "astrocommunity.pack.scala" },
   { import = "astrocommunity.test.neotest" },
+  { import = "astrocommunity.pack.go" },
+  { import = "astrocommunity.pack.xml" },
   -- import/override with your plugins folder
 }
