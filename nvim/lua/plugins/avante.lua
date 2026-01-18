fec28d52f1cf6da1bae6ec3a5f47ffc3a009faf4
Maxamillion <tmax898@gmail.com>
2026-01-18 13:37:00 -0600

    updates and setup avante for acp

--- nvim/lua/plugins/avante.lua
+++ nvim/lua/plugins/avante.lua
@@ -2,7 +2,7 @@ return {
   "yetone/avante.nvim",
   event = "VeryLazy",
   opts = {
-    provider = "gemini", -- This sets Gemini as the main provider
+    provider = "gemini", -- This should set the provider
     providers = {
       gemini = {
         model = "gemini-2.5-flash",
@@ -17,6 +17,18 @@ return {
         __inherited_from = "gemini",
         model = "gemini-2.5-pro",
         display_name = "Gemini 2.5 Pro",
+      },
+    },
+    -- Do not use gemini-cli as a provider until https://github.com/google-gemini/gemini-cli/issues/14986 and the bug related to hit has been resolved
+    acp_providers = {
+      ["gemini-cli"] = {
+        command = "gemini",
+        model = "gemini-2.5-flash",
+        args = { "--experimental-acp" },
+        env = {
+          NODE_NO_WARNINGS = "1",
+          GEMINI_API_KEY = os.getenv "AVANTE_GEMINI_API_KEY",
+        },
       },
     },
   },
