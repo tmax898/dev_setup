ad5d6ef2833effe816232db15a6899a3ccf02965
Maxamillion <tmax898@gmail.com>
2026-04-29 00:25:06 -0500

    Update to nvim v6 (#3)
    * update to v6, i think i got most of the parity back
    
    * remove comm.origami, remove hack for auto collapse
    
    * fix naming and dupe

--- nvim/lua/community.lua
+++ nvim/lua/community.lua
@@ -8,7 +8,6 @@
 return {
   "AstroNvim/astrocommunity",
   { import = "astrocommunity.pack.lua" },
-  { import = "astrocommunity.colorscheme.catppuccin" },
   { import = "astrocommunity.colorscheme.night-owl-nvim" },
   { import = "astrocommunity.motion.nvim-surround" },
   { import = "astrocommunity.media.neocord-nvim" },
@@ -23,6 +22,6 @@ return {
   { import = "astrocommunity.pack.xml" },
   -- { import = "astrocommunity.pack.elixir-phoenix" },
   -- { import = "astrocommunity.editing-support.mcphub-nvim" },
-  -- { import = "astrocommunity.completion.avante-nvim" },
+  -- { import = "astrocommunity.editing-support.nvim-origami" },
   -- import/override with your plugins folder
 }
--- nvim/lua/plugins/nvim-ufo.lua
+++
@@ -1,6 +0,0 @@
-return {
-  "kevinhwang91/nvim-ufo",
-  opts = {
-    close_fold_kinds_for_ft = { default = { "imports" } },
-  },
-}
